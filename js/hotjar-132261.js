abeacccf58f4f08c99a0923b7b844f810e77b441
Daniel <thedilligent@gmail.com>
2016-04-05 22:03:48 -0400

    Added files via upload

---
+++ js/hotjar-132261.js
@@ -0,0 +1,9 @@
+window.hjSiteSettings = {"testers_widgets":[],"surveys":[],"heatmaps":[],"recording_capture_keystrokes":true,"polls":[],"site_id":132261,"record_targeting_rules":[],"forms":[],"record":false,"r":1.0,"deferred_page_contents":[]};
+
+window.hjBootstrap = function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+};
+
+hjBootstrap('https://script.hotjar.com/modules-d7af38b5aa5145ab899bd263964bf1f5.js');
